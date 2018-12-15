365d6debcb9da3d774efa17b3457e93499166aff
C2H5OHlife <zhoulingtao7658@Hotmail.com>
2018-12-15 19:00:01 +0800

    take some note

--- models/PoseExpNet.py
+++ models/PoseExpNet.py
@@ -70,7 +70,7 @@ class PoseExpNet(nn.Module):
 
         pose = self.pose_pred(out_conv7)
         pose = pose.mean(3).mean(2)
-        pose = 0.01 * pose.view(pose.size(0), self.nb_ref_imgs, 6)
+        pose = 0.01 * pose.view(pose.size(0), self.nb_ref_imgs, 6)  # 为什么 x 0.01
 
         if self.output_exp:
             out_upconv5 = self.upconv5(out_conv5  )[:, :, 0:out_conv4.size(2), 0:out_conv4.size(3)]
